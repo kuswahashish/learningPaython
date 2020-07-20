d666bae3f21e1bea8e0822f8b1ab4a962d11c489
kuswahashish <kuswahashish@gmail.com>
2020-07-20 13:27:07 +0530

    learning String in paython

---
+++ formatted_String.py
@@ -0,0 +1,7 @@
+first = 'Ashish'
+last = 'Kuswah'
+message = first + '[' + last + '] is a Enginner ;)'
+print(message)
+# not good way to do we should use frmatted string :)
+msg = f'{first}[{last}] is an Engineer :)'
+print(msg)
---
+++ string.py
@@ -0,0 +1,12 @@
+course = "Paython's Course for Beginners"#its for using '' "" in string
+print(course)
+#
+#course1 = '''Hello ,
+#How are you ?
+#Do What you like!
+#'''
+#print(course1)
+course2 = 'Hello'
+print(course2[0])#this will print first character
+print(course2[-1])#this will print last one
+print(course2[0:4])#it will print 0 to 4 index character

5a050dd6ba0d9f16082ee96fd61aa9c678394e47
kuswahashish <kuswahashish@gmail.com>
2020-07-20 14:10:18 +0530

    learning StringMethods in paython

---
+++ StringMethod.py
@@ -0,0 +1,8 @@
+course = 'Paython Learning'
+print(len(course)) #len function for finding length of string
+#do course. and you will get almost all the string methods
+print(course.upper())
+print(course.lower())
+print(course.find('L'))
+print(course.replace('dLearning','language Learning'))
+print('Paython' in course)

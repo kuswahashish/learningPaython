d9600aaeb00e81c61d91c9b3e56e5804ea10622b
kuswahashish <kuswahashish@gmail.com>
2020-08-24 12:40:10 +0530

    learned for loop

---
+++ forLoop.py
@@ -0,0 +1,12 @@
+#for item in 'Paython':
+#    print(item)
+# We can use the list or numbers here
+#for item in ['Paython','Java','HTML','JScript','c++']:
+#    print(item)
+
+#total of shooping cart
+total = 0
+price = [120,50,80,70,60]
+for item in price:
+    total= total + item
+print("total amount is : ",total)
